a54017f249266f0a240f309738be65f6b81a4b03
unknown <jakgrit_frame@hotmail.com>
2019-01-18 17:24:33 +0700

    edit

--- Queue.test.js
+++ Queue.test.js
@@ -10,26 +10,26 @@ test('สร้าง queue ใหม่และทดสอบว่า empty 
 // 2. สร้าง queue size 1 และ ทดสอบว่าสามารถ enqueue(20) และ dequeue ค่าที่ได้จะได้ 20
 test('สร้าง queue size 1 และ ทดสอบว่าสามารถ enqueue(20) และ dequeue ค่าที่ได้จะได้ 20',()=> {
     const queue = new Queue(1);
-    expect(queue.enqueue(20)).toBe(queue.dequeue() == 20);
+    expect(queue.enqueue(20)).toBe(20);
 });
 
 // 3. สร้าง queue size 1 และ ทดสอบว่า enqueue เข้าไป 1 ตัว แล้ว full จะได้ค่า true (เต็ม)
 test('สร้าง queue size 1 และ ทดสอบว่า enqueue เข้าไป 1 ตัว แล้ว full จะได้ค่า true (เต็ม)',()=> {
     const queue = new Queue(1);
-    expect(queue.enqueue(1)).toBe(queue.full() == true);
+    expect(queue.enqueue(1)).toBe(true);
 });
 
 // 4. สร้าง queue size 1 และ ทดสอบว่า เมื่อเพิ่มมากกว่า 1 แล้ว enqueue(5) ค่าที่ได้จะเป็น false (เพิ่มไม่ได้)
 test('สร้าง queue size 1 และ ทดสอบว่า เมื่อเพิ่มมากกว่า 1 แล้ว enqueue(5) ค่าที่ได้จะเป็น false (เพิ่มไม่ได้)',()=> {
     const queue = new Queue(1);
-    expect(queue.enqueue(5)).toBe(queue.enqueue(5) == false);
+    expect(queue.enqueue(5)).toBe(false);
 });
 
 // 5. สร้าง queue size 2 และ enqueue 1 และ 2 เข้าไป เมื่อ dequeue จะได้ ค่าเรียงลำดับเป็น 1 และ 2 เช่นกัน
 test('สร้าง queue size 2 และ enqueue 1 และ 2 เข้าไป เมื่อ dequeue จะได้ ค่าเรียงลำดับเป็น 1 และ 2 เช่นกัน',()=> {
     const queue = new Queue(2);
-    expect(queue.enqueue(1)).toBe(queue.dequeue() == 1);
-    expect(queue.enqueue(2)).toBe(queue.dequeue() == 2);
+    expect(queue.enqueue(1)).toBe(1);
+    expect(queue.enqueue(2)).toBe(2);
 });
 
 // 6. สร้าง queue size 1 และ dequeue เลย จะต้องตรวจว่าค่าที่ได้จาก dequeue เป็น null
